f676d79a13b43d6ce6e450622984d699f6e5a681
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-10-11 16:18:35 +0900

    [Bronze III] Title: 직각삼각형, Time: 64 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -1,11 +1,10 @@
 while True:
     length = list(map(int, input().split()))
-    bit = max(length) # 빗변
-    length_1 = length.copy()
-    length_1.remove(bit)
+    # sort() 사용하기
+    length.sort()
     if sum(length) == 0:
         break
-    elif (length_1[0]**2)+(length_1[1]**2) == (bit**2):
+    elif (length[0]**2)+(length[1]**2) == (length[2]**2):
         print('right')
     else:
         print('wrong')
