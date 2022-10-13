637b28f00d10bfc1e8ebe4b66484173e8686d0d2
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-10-13 22:10:13 +0900

    [unrated] Title: 모음 제거, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.py
@@ -2,7 +2,8 @@ def solution(my_string):
     
     alpha_mo = ['a', 'e', 'i', 'o', 'u']
     for i in alpha_mo:
-
-        my_string = my_string.replace(i, '')
+        #예외 처리(replace는 예외가 발생해도 오류 x)
+        if i in my_string:
+            my_string = my_string.replace(i, '')
          
     return my_string
