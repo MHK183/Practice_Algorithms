02e2167bfae0769ad0e7f5cee75517d64e49c2b1
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-10-19 19:14:33 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.py
@@ -1,9 +1,8 @@
 def solution(spell, dic):
     answer = 2
+    spell = set(spell)
     for i in dic:
-        if len(i) >= len(spell):
-            for j in spell:
-                i = i.replace(j, '', 1)
-            if len(i) == 0:
-                answer = 1
+        if not spell - set(i):
+            answer = 1
+            break
     return answer
