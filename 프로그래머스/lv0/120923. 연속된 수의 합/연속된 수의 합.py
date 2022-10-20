4209092482642331c1f5b34210557709ff8fed87
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-10-20 21:17:04 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.73 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,5 +1,5 @@
 def solution(num, total):
-    answer = [i for i in range(total//num - num,total//num)]
+    answer = [i for i in range(total//num - num+1,total//num+1)]
     while sum(answer) != total:
         for i in range(num):
             answer[i] = answer[i]+1
