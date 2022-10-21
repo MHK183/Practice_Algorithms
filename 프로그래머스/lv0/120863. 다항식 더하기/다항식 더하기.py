4248a970059baac7170700e44afda1f00fa046cd
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-10-21 11:38:18 +0900

    [level 0] Title: 다항식 더하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.py
@@ -0,0 +1,21 @@
+def solution(polynomial):
+    a = [i for i in polynomial.split()[0::2]]
+    x = 0
+    digit = 0
+    for k in a:
+        if 'x' in k:
+            k = k.replace('x','')
+            if k == '':
+                x += 1
+            else:
+                x += int(k)
+        else:
+            digit += int(k)
+
+    if x == 1:
+        x = ''
+    if x == 0:
+        return f'{digit}'
+    if digit == 0:
+        return f'{x}x'
+    return f'{x}x + {digit}'
