6bc5e39e2bc3ef8a836a57f381980c590c8ee134
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-10-26 19:29:42 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -1,6 +1,6 @@
 def solution(a, b):
-    weeks = ['THU','FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']
+    weeks = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED','THU']
     month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     day = sum(month[:a-1])
-    day += b
+    day += b - 1 
     return weeks[day % 7]
