2d125dc344690aa1a139a808d76233f34651f75d
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-10-27 15:46:59 +0900

    [level 1] Title: 모의고사, Time: 2.98 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,32 +1,19 @@
 def solution(answers):
-    answer = []
+    result = []
     s1 = [1,2,3,4,5]
     s2 = [2,1,2,3,2,4,2,5]
     s3 = [3,3,1,1,2,2,4,4,5,5]
-    s1_score, s2_score, s3_score = 0, 0, 0
-
-    l = len(answers)
-    for i in range(l):
-        k = i
-        i %= len(s1)
-        if s1[i] == answers[k]:
-            s1_score += 1
+    scores = [0, 0, 0]
+    # 나머지를 이용해 반복
+    for idx, answer in enumerate(answers):
+        if s1[idx % len(s1)] == answer:
+            scores[0] += 1
+        if s2[idx % len(s2)] == answer:
+            scores[1] += 1
+        if s3[idx % len(s3)] == answer:
+            scores[2] += 1
             
-        i = k
-        
-        i %= len(s2)
-        if s2[i] == answers[k]:
-            s2_score += 1
-            
-        i = k
-        
-        i %= len(s3)
-        if s3[i] == answers[k]:
-            s3_score += 1
-            
-    scores = [s1_score, s2_score, s3_score]
-
     for i in range(len(scores)):
         if max(scores) == scores[i]:
-            answer.append(i+1)
-    return answer
+            result.append(i+1)
+    return result
