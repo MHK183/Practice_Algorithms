2c14890ea5ce36ca2256d9f1901d8bdd2b981046
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-10-28 15:39:57 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.py
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.py
@@ -1,7 +1,4 @@
 n = int(input())
 
-for i in range(1, n+1):
-    for j in range(1,n+1):
-        print('*'+' ', end='')
-    print()
-    print(' '*(i%2), end='')
+for i in range(n):
+    print(' '*(i%2) + ('*'+' ')*n)
