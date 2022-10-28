0c6a83d41e0b2c2f7b3aa9eaf8fb8392c51fea66
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-10-28 21:31:31 +0900

    [Bronze II] Title: 24, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1408. 24/24.py
+++ 백준/Bronze/1408. 24/24.py
@@ -1,6 +1,5 @@
 a = list(map(int, input().split(':')))
 b = list(map(int, input().split(':')))
-time = []
 x = a[0] * 3600 + a[1] * 60 + a[2]
 
 if a[0] >= b[0]:
@@ -13,26 +12,5 @@ h, m, s = 0, 0, 0
 s += result
 m += s // 60
 h += m // 60
-
-if h%24 < 10:
-    if m%60 < 10:
-        if s%60 < 10:
-            print(f'0{h%24}:0{m%60}:0{s%60}')
-        else:
-            print(f'0{h%24}:0{m%60}:{s%60}')
-    else:
-        if s%60 < 10:
-            print(f'0{h%24}:{m%60}:0{s%60}')
-        else:
-            print(f'0{h%24}:{m%60}:{s%60}')
-else:
-    if m%60 < 10:
-        if s%60 < 10:
-            print(f'{h%24}:0{m%60}:0{s%60}')
-        else:
-            print(f'{h%24}:0{m%60}:{s%60}')
-    else:
-        if s%60 < 10:
-            print(f'{h%24}:{m%60}:0{s%60}')
-        else:
-            print(f'{h%24}:{m%60}:{s%60}')
+# f string :02d
+print(f'{h%24:02d}:{m%60:02d}:{s%60:02d}')
