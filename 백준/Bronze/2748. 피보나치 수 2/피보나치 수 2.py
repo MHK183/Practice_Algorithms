5896034c458a642bb6a76514b778e97a4aeca49a
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-10-28 09:41:36 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,12 +1,5 @@
 n = int(input())
-idx = 0
-fn_1 = 1
-fn_2 = 0
-while n != idx:
-    idx += 1
-    
-    fn = fn_2 + fn_1
-    fn_2 = fn_1
-    fn_1 = fn
-    
-print(fn_2)
+fn = [0, 1]
+for i in range(n-1):
+    fn.append(fn[i]+fn[i+1])
+print(fn[n])
