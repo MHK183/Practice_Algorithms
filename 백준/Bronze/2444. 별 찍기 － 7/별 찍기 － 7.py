93d611e61c3e4c91851816e30f15cbe874f2fb30
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-10-28 14:04:31 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,9 +1,7 @@
 n = int(input())
 output = ''
 
-for i in range(1, n+1):
-    output += ' '*(n - i) + '*'*(2*i-1) + '\n'
-for j in range(n-1, 0, -1):
-    output += ' '*(n - j) + '*'*(2*j-1) + '\n'
-    
+for i in range(-n+1, n):
+    i = abs(i)
+    output += ' '*i + '*'*(2*(n-i)-1) + '\n'
 print(output)
