c59ddc2a4c7a0223f5982cf792c77abc9dbf5464
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-10-29 16:03:45 +0900

    [Silver IV] Title: 동전 0, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,15 @@
+n , k = map(int, input().split())
+coins = []
+count = 0
+for _ in range(n):
+    coins.append(int(input()))
+coins.sort(reverse=True)
+
+
+for coin in coins:
+    if coin > k:
+        continue
+    else:
+        count += k // coin
+        k %= coin
+print(count)
