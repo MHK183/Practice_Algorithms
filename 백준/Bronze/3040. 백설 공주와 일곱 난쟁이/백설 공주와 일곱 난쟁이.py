39fd721a46cc283979ae0924be159b15d38f9b34
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-10-29 16:35:36 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.py
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.py
@@ -1,13 +1,12 @@
 a = [int(input()) for _ in range(9)]
 b = a.copy()
+l = len(a)
 
-for idx, i in enumerate(a[:-1]):
-    for j in a[idx+1:]:
-        if (i+j) == (sum(a) - 100):
-            b.remove(i)
-            b.remove(j)
+for i in range(l-1):
+    for j in range(i+1, l):
+        if (a[i]+a[j]) == (sum(a) - 100):
+            b.remove(a[i])
+            b.remove(a[j])
             break
 for k in b:
     print(k)
-            
-    
