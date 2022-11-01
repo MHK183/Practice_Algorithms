b7d3c2a7d6e12ea153be8947a4c82beabac893b5
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-11-01 13:45:14 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 404 ms, Memory: 57760 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -1,6 +1,10 @@
+import sys
+
 coordinate = []
+
 for _ in range(int(input())):
-    coordinate.append(list(map(int, input().split())))
+    coordinate.append(list(map(int, sys.stdin.readline().split())))
 coordinate.sort(key = lambda x : (x[1], x[0]))
+
 for i in coordinate:
     print(i[0], i[1])
