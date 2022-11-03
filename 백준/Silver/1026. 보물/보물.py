88cbc72be9e10bc6a81c291e08370b730ea2d076
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-11-03 10:40:06 +0900

    [Silver IV] Title: 보물, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -1,8 +1,9 @@
 n = int(input())
 a = sorted(list(map(int, input().split())))
-b = sorted(list(map(int, input().split())), reverse=True)
+b = list(map(int, input().split()))
 answer = 0
-
+# max값을 곱하고 제거해주기 (b 정렬하면 안되는 조건 지키기)
 for i in range(n):
-    answer += a[i] * b[i]
+    answer += a[i] * max(b)
+    b.remove(max(b))
 print(answer)
