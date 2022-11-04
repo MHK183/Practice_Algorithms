9e7d49c81597c3f5f2d4bb896b751f33e347a15f
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-11-04 10:54:33 +0900

    [Bronze II] Title: 파일 완전 삭제, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/9243. 파일 완전 삭제/파일 완전 삭제.py
+++ 백준/Bronze/9243. 파일 완전 삭제/파일 완전 삭제.py
@@ -1,6 +1,6 @@
 n = int(input())
 before, after = int(input(), 2), int(input(), 2)
-result = bin(before ^ after)[2:]
+result = bin(before + after)[2:]
 
 if n % 2 == 1:
     if result.count('0') == 0:
