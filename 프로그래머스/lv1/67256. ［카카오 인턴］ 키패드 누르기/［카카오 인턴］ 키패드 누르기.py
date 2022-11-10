6de52eb8cc3758bc4e19bb599399d462b3b41dd7
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-11-10 18:22:49 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.44 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -0,0 +1,39 @@
+def solution(numbers, hand):
+    answer = ''
+    key = [(0,1),(3,0),(3,1),(3,2),(2,0),(2,1),(2,2),(1,0),(1,1),(1,2)]
+    right_num = [3,6,9]
+    left_num = [1,4,7]
+    mid_num = [0,2,5,8]
+    
+    left_loc = (0,0)
+    right_loc = (0,2)
+    
+    for number in numbers:
+    
+        if number in mid_num:
+            if abs(key[number][0] - left_loc[0]) + abs(key[number][1] - left_loc[1]) < \
+                abs(key[number][0] - right_loc[0]) + abs(key[number][1] - right_loc[1]):
+                answer += 'L'
+                left_loc = key[number]
+            elif abs(key[number][0] - left_loc[0]) + abs(key[number][1] - left_loc[1]) > \
+                abs(key[number][0] - right_loc[0]) + abs(key[number][1] - right_loc[1]):
+                answer += 'R'
+                right_loc = key[number]
+            else:
+                if hand == 'right':
+                    answer += 'R'
+                    right_loc = key[number]
+                elif hand == 'left':
+                    answer += 'L'
+                    left_loc = key[number]
+                
+        elif number in right_num:
+            answer += 'R'
+            right_loc = key[number]
+        elif number in left_num:
+            answer += 'L'
+            left_loc = key[number]
+    
+    
+    
+    return answer
