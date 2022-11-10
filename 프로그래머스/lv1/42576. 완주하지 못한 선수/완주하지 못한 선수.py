fd3f5233e4ab90032d239257c09ff5c56e7b5fcb
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-11-10 14:13:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.44 ms, Memory: 26.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,9 @@
+def solution(participant, completion):
+
+    participant.sort()
+    completion.sort()
+    completion.append(0)
+    for i in range(len(participant)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    
