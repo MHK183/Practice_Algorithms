23548a3125b7dd1b8ec29bb09a5c8b323b140fd1
MHK183 <102278456+MHK183@users.noreply.github.com>
2022-11-10 18:28:54 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,7 +1,6 @@
 def solution(k, m, score):
     answer = 0
     score.sort(reverse=True)
-    apple_box = []
     for i in range(0, len(score), m):
         if len(score[i:i+m]) == m:
             answer += min(score[i:i+m]) * m
