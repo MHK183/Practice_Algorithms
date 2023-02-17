f9ced80a68642e413a31f13da222a0fc4f6ef053
MHK183 <102278456+MHK183@users.noreply.github.com>
2023-02-17 18:13:11 +0900

    [Bronze II] Title: 하얀 칸, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1100. 하얀 칸/하얀 칸.py
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -1,8 +1,5 @@
 answer = 0
-for i in range(1, 8 + 1):
+for i in range(8):
     chess_board = input()
-    if i % 2 == 1:
-        answer += chess_board[0:8:2].count('F')
-    else:
-        answer += chess_board[1:8:2].count('F')
+    answer += chess_board[i%2:8:2].count('F')
 print(answer)
