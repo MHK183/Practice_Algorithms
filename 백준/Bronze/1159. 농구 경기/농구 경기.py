8d794dbfe87732b7c4e0c559a119555ea2c0b292
MHK183 <102278456+MHK183@users.noreply.github.com>
2023-02-27 19:58:31 +0900

    [Bronze II] Title: 농구 경기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.py
@@ -0,0 +1,13 @@
+N = int(input())
+members = [input()[0] for _ in range(N)]
+alpha = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+answer = ''
+
+
+for i in alpha:
+    if members.count(i) >= 5:
+        answer += i
+if answer == '':
+    print('PREDAJA')
+else:
+    print(answer)
